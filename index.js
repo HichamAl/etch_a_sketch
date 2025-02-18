0fcebd7374abdb7c596bc0c3e8d7c39e40f284b3
HichamAl <104017860+HichamAl@users.noreply.github.com>
2025-02-18 19:31:37 +0100

    new grid creation works on user input

--- index.js
+++ index.js
@@ -16,7 +16,41 @@ for (let i = 0; i < 256; i++){
 
     square.addEventListener("mouseover", ()=>{
         square.style.backgroundColor = `red`;
-    });
+    });    
 }
+
+// add later, make sure user can only put in a number not a string 
+// max of 100 squares also 
+const button = document.querySelector("#getGridSize");
+button.addEventListener("click", ()=> {
+    const squareAmount = prompt("Give a number for the amount of squares");
+    const amountOfDivs = squareAmount * squareAmount;
+
+    // remove divs
+    const container = document.querySelector("#container");
+    for (let i = 0; i < 256; i++){
+        const div = container.firstElementChild;
+        container.removeChild(div);
+    }
+
+    // create new grid
+    for (let i = 0; i < amountOfDivs; i++){
+        const square = document.createElement("div");
+        square.style.backgroundColor = "yellow";
+        square.style.border = "thin solid #00FF00";  
+        let squareSize = 832 / squareAmount -2 ;
+
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
+
+        container.appendChild(square); 
+
+        square.addEventListener("mouseover", ()=>{
+            square.style.backgroundColor = `red`;
+        });    
+    }
+    
+});
+
 
 
